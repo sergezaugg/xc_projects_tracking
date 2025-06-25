86655e4fed30c9ea5fc74232e0d739e3eb09c21a
Serge Zaugg <serge.zaugg@gmail.com>
2025-06-25 12:50:35 +0200

    misc

--- fe_main_batch.py
+++ fe_main_batch.py
@@ -9,8 +9,66 @@
 # Usage example: python main_batch.py -i "./dev_data/images" -n 31
 #--------------------
 
-# pip install https://github.com/sergezaugg/feature_extraction_idnn/releases/download/v0.9.2/fe_idnn-0.9.2-py3-none-any.whl
+
+
+
+# pip install https://github.com/sergezaugg/feature_extraction_idnn/releases/download/v0.9.3.1/fe_idnn-0.9.3.1-py3-none-any.whl
+
+# pip install https://github.com/sergezaugg/feature_extraction_saec/releases/download/v0.9.4/fe_saec-0.9.4-py3-none-any.whl
+
 # pip install torch torchvision --index-url https://download.pytorch.org/whl/cu126
+# pip install --force-reinstall torch torchvision --index-url https://download.pytorch.org/whl/cu126
+
+
+import torch
+
+from fe_saec import SAEC_extractor
+from fe_idnn import IDNN_extractor
+
+
+torch.cuda.is_available()
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+image_path = "D:/xc_real_projects/xc_parus_01/xc_spectrograms"
+
+# SAEC
+# Initialize a AEC-extractor instance
+path_models = "D:/xc_real_projects/pytorch_hot_models_keep"
+model_tag = "20250617_150956"
+se = SAEC_extractor(path_models, model_tag, path_images = image_path, device = device)
+# extract (will save to disk as npz)
+se.extract(devel = True)
+# time pool
+se.time_pool()
+# dim reduce
+[se.reduce_dimension(n_neigh = 10, reduced_dim = d) for d in [2,4,8,16]]
+
+# IDNN 
+n_batches = 10
+ie = IDNN_extractor(model_tag = "ResNet50")
+ie.eval_nodes
+ie.create("layer1.2.conv3")
+ie.extract(image_path, freq_pool = 4, batch_size = 16, n_batches = n_batches, ecut = 1)
+[ie.reduce_dimension(n_neigh = 10, reduced_dim = d) for d in [2,4,8,16]]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 #-----------------------------------
 # main process starts here 
@@ -67,6 +125,15 @@ fe.eval_nodes
 fe.create("blocks.3.layers.1.layers.MBconv.layers.conv_c")
 fe.extract(image_path, freq_pool = 1, batch_size = 16, n_batches = n_batches, ecut = 1)
 [fe.reduce_dimension(n_neigh = 10, reduced_dim = d) for d in [2,4,8,16]]
+
+
+
+
+
+
+
+
+
 
 
 
