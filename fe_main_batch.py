d817d055dafec6c6b0b8bc04d99a09c1f505344d
Serge Zaugg <serge.zaugg@gmail.com>
2025-06-25 16:46:41 +0200

    misc

--- fe_main_batch.py
+++ fe_main_batch.py
@@ -3,7 +3,7 @@
 # Description : 
 #--------------------
 
-# pip install --upgrade https://github.com/sergezaugg/feature_extraction_idnn/releases/download/v0.9.4/fe_idnn-0.9.4-py3-none-any.whl
+# pip install --upgrade https://github.com/sergezaugg/feature_extraction_idnn/releases/download/v0.9.5/fe_idnn-0.9.5-py3-none-any.whl
 # pip install --upgrade https://github.com/sergezaugg/feature_extraction_saec/releases/download/v0.9.5/fe_saec-0.9.5-py3-none-any.whl
 
 # pip install torch torchvision --index-url https://download.pytorch.org/whl/cu126
@@ -17,6 +17,9 @@ torch.cuda.is_available()
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 image_path = "D:/xc_real_projects/xc_parus_01/xc_spectrograms"
+image_path = "D:/xc_real_projects/xc_sw_europe/xc_spectrograms"
+
+
 
 # SAEC
 path_model = "D:/xc_real_projects/pytorch_hot_models_keep/20250617_150956_encoder_script_GenC_new_TP32_epo007.pth"
@@ -31,7 +34,7 @@ ie = IDNN_extractor(model_tag = "ResNet50")
 ie.eval_nodes
 ie.create("layer1.2.conv3")
 # extract 
-ie.extract(image_path, freq_pool = 4, batch_size = 16, n_batches = 10, ecut = 1)
+ie.extract(image_path = image_path, freq_pool = 4, batch_size = 16, n_batches = 10, ecut = 1)
 [ie.reduce_dimension(n_neigh = 10, reduced_dim = d) for d in [2,4,8,16]]
 
 
