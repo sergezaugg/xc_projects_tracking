191eef5be6cb02dc68ef08729b07179c72e01f6e
Serge Zaugg <serge.zaugg@gmail.com>
2025-07-15 20:39:11 +0200

    misc

--- 03_feature_extraction/fe_main_batch.py
+++ 03_feature_extraction/fe_main_batch.py
@@ -12,28 +12,69 @@ from fe_saec import SAEC_extractor
 torch.cuda.is_available()
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-# path_source_images = "D:/xc_real_projects/xc_sw_europe/xc_spectrograms"
-# path_save_features = "C:/Users/sezau/Downloads/xc_sw_europe"
-
-# path_source_images = "D:/xc_real_projects/xc_data_02_Corvidae/images_24000sps_20250708_091750"
-# path_save_features = "C:/Users/sezau/Downloads/xc_corvus_corax"
-
+# path_source_images = "D:/xc_real_projects/xc_data_history/xc_sw_europe/xc_spectrograms"
+# path_save_features = "D:/xc_real_projects/xc_data_history/xc_sw_europe/extracted_features"
 
 # path_source_images = "D:/xc_real_projects/xc_data_history/xc_parus_01/xc_spectrograms"
 # path_save_features = "D:/xc_real_projects/xc_data_history/xc_parus_01/extracted_features"
 
-path_source_images = "D:/xc_real_projects/xc_data_02_Corvidae/images_24000sps_20250708_091750"
+path_source_images = "D:/xc_real_projects/xc_data_02_Corvidae/xc_spectrograms"
 path_save_features = "D:/xc_real_projects/xc_data_02_Corvidae/extracted_features"
-
-
 
 if not os.path.exists(path_save_features):
     os.mkdir(path_save_features) 
 
-# n_batches = 5
-n_batches = 1000000000
-
 basiz = 64
+
+
+#-----------
+# SAEC
+
+# "D:\xc_real_projects\pytorch_models\conv_tran_L5_TP32_ch256"
+# "D:\xc_real_projects\pytorch_models\conv_tran_L5_sym"
+# "D:\xc_real_projects\pytorch_models\conv_tran_texture_01"
+# "D:\xc_real_projects\pytorch_models\conv_tran_textr_resh"
+
+# DEF conv_tran_L5_TP32_ch256
+path_model = "D:/xc_real_projects/pytorch_models/conv_tran_L5_TP32_ch256/20250710_223517_encoder_script_conv_tran_L5_TP32_epo006.pth"
+ae = SAEC_extractor(path_model = path_model, device = device) 
+ae.extract(image_path = path_source_images, fe_save_path = path_save_features, batch_size = basiz, shuffle = True , n_batches = n_batches) 
+ae.time_pool(ecut=2)
+[ae.reduce_dimension(n_neigh = 10, reduced_dim = d) for d in [2,4,8,16]]  
+
+
+
+
+
+# NEW conv_tran_L5_sym (success : done 20250715 - 20:29)
+path_model = "D:/xc_real_projects/pytorch_models/conv_tran_L5_sym/20250711_154215_encoder_script_conv_tran_L5_sym_epo006.pth"
+ae = SAEC_extractor(path_model = path_model, device = device) 
+ae.extract(image_path = path_source_images, fe_save_path = path_save_features, batch_size = basiz, shuffle = True , n_batches = None, verbose = True) 
+ae.time_pool(ecut=2)
+[ae.reduce_dimension(n_neigh = 10, reduced_dim = d) for d in [2,4,8,16]]  
+
+# NEW conv_tran_texture_01 (failed: memory issues with  ae.time_pool(ecut=2))
+path_model = "D:/xc_real_projects/pytorch_models/conv_tran_texture_01/20250713_224421_encoder_script_conv_tran_texture_01_epo004.pth"
+ae = SAEC_extractor(path_model = path_model, device = device) 
+ae.extract(image_path = path_source_images, fe_save_path = path_save_features, batch_size = basiz, shuffle = True , n_batches = None, verbose = True) 
+# to free max of memory the hard way: - close session and start new session + run code below 
+ae.load_full_features_from_npz(path_npz = os.path.join(path_save_features, "full_features_saec_20250713_224421.npz"))
+ae.time_pool(ecut=2)
+[ae.reduce_dimension(n_neigh = 10, reduced_dim = d) for d in [2,4,8,16]]  
+
+# NEW conv_tran_textr_resh (success : done 20250715 - 19:26)
+path_model = "D:/xc_real_projects/pytorch_models/conv_tran_textr_resh/20250714_090709_encoder_script_conv_tran_textr_resh_epo006.pth"
+ae = SAEC_extractor(path_model = path_model, device = device) 
+ae.extract(image_path = path_source_images, fe_save_path = path_save_features, batch_size = basiz, shuffle = True , n_batches = None, verbose = True)
+# to free max of memory the hard way: - close session and start new session + run code below 
+# ae.load_full_features_from_npz(path_npz = os.path.join(path_save_features, "full_features_saec_20250714_090709.npz"))
+ae.time_pool(ecut=2)
+[ae.reduce_dimension(n_neigh = 10, reduced_dim = d) for d in [2,4,8,16]]  
+
+
+
+
+
 
 
 
@@ -88,52 +129,3 @@ fe.create("features.28") #  block 5
 fe.extract(image_path = path_source_images, fe_save_path = path_save_features,  freq_pool = 4, batch_size = basiz, n_batches = n_batches, ecut = 1)
 [fe.reduce_dimension(n_neigh = 10, reduced_dim = d) for d in [2,4,8,16]]
 
-#-----------
-# SAEC
-
-# DEF good models 
-path_model = "D:/xc_real_projects/pytorch_models/conv_tran_L5_TP32_ch256/20250710_223517_encoder_script_conv_tran_L5_TP32_epo006.pth"
-ae = SAEC_extractor(path_model = path_model, device = device) 
-ae.extract(image_path = path_source_images, fe_save_path = path_save_features, batch_size = basiz, shuffle = True , n_batches = n_batches) 
-ae.time_pool(ecut=2)
-[ae.reduce_dimension(n_neigh = 10, reduced_dim = d) for d in [2,4,8,16]]  
-
-# NEW conv_tran_L5_sym
-path_model = "D:/xc_real_projects/pytorch_models/conv_tran_L5_sym/20250711_154215_encoder_script_conv_tran_L5_sym_epo006.pth"
-ae = SAEC_extractor(path_model = path_model, device = device) 
-ae.extract(image_path = path_source_images, fe_save_path = path_save_features, batch_size = basiz, shuffle = True , n_batches = n_batches) 
-ae.time_pool(ecut=2)
-[ae.reduce_dimension(n_neigh = 10, reduced_dim = d) for d in [2,4,8,16]]  
-
-# NEW conv_tran_texture_01
-path_model = "D:/xc_real_projects/pytorch_models/conv_tran_texture_01/20250713_224421_encoder_script_conv_tran_texture_01_epo004.pth"
-ae = SAEC_extractor(path_model = path_model, device = device) 
-ae.extract(image_path = path_source_images, fe_save_path = path_save_features, batch_size = basiz, shuffle = True , n_batches = n_batches) 
-ae.time_pool(ecut=2)
-[ae.reduce_dimension(n_neigh = 10, reduced_dim = d) for d in [2,4,8,16]]  
-
-
-
-
-
-
-
-
-
-# NEW conv_tran_textr_resh
-path_model = "D:/xc_real_projects/pytorch_models/conv_tran_textr_resh/20250713_232131_encoder_script_conv_tran_textr_resh_epo003.pth"
-ae = SAEC_extractor(path_model = path_model, device = device) 
-ae.extract(image_path = path_source_images, fe_save_path = path_save_features, batch_size = basiz, shuffle = True , n_batches = n_batches, verbose = True)
-
-# to free max of memory the hard way: - close session and start new session + run code below 
-# ae.load_full_features_from_npz(path_npz = "D:/xc_real_projects/xc_data_history/xc_parus_01/extracted_features/full_features_saec_20250713_232131.npz")
-ae.load_full_features_from_npz(path_npz = "D:/xc_real_projects/xc_data_02_Corvidae/extracted_features/full_features_saec_20250713_232131.npz")
-
-ae.time_pool(ecut=2)
-[ae.reduce_dimension(n_neigh = 10, reduced_dim = d) for d in [2,4,8,16]]  
-
-# gc.collect()
-# torch.cuda.empty_cache()
-# torch.cuda.ipc_collect()
-# print(torch.cuda.memory_allocated())
-# print(torch.cuda.memory_reserved())
